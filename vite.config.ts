8a5f501b442ddc4df42d6ef2dd1952749340d971
Krishna Kant Rawat <rawatkrishnakant8319@gmail.com>
2025-01-15 23:01:14 +0530

    new changes added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// filepath: /Users/krishnakantrawat/Documents/learning/reactjs/first-vite-react-app/vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "build", // Ensure this matches the directory specified in the deploy script
+  },
+  base: "/nextjs-app/", // This should match your repository name
+});
